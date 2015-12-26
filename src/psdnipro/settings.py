c05961cb66b25584c08c4905b6de2f20199bee8a
Victor Apkarian <victor.apkarian@gmail.com>
2015-12-27 01:46:27 +0200

    notifications about 404

--- src/psdnipro/settings.py
+++ src/psdnipro/settings.py
@@ -55,6 +55,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE_CLASSES = [
+    'django.middleware.common.BrokenLinkEmailsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
